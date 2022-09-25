296bbc08ab6445a93ec4d6bc1d40cdf7b60c46ca
khanhdii-08 <phamduy2408@gmail.com>
2022-09-25 19:49:10 +0700

    add delete

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const multer = require("multer");
 const docClient = require("./configAWS");
+
+//middleware
 const app = express();
+const convertFormToJson = multer();
 
 app.use(express.json({ extended: false }));
 app.use(express.static("./views"));
@@ -18,6 +22,40 @@ app.get("/", (req, res) => {
   docClient.scan(params, (err, data) => {
     if (err) res.send("err");
     else return res.render("index", { sanPhams: data.Items });
+  });
+});
+
+app.post("/", convertFormToJson.fields([]), (req, res) => {
+  const { id, name, quantity } = req.body;
+  console.log();
+  const params = {
+    TableName: tableName,
+    Item: {
+      id: Number(id),
+      name,
+      quantity,
+    },
+  };
+  console.log(id, name, quantity);
+  docClient.put(params, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      return res.redirect("/");
+    }
+  });
+});
+app.post("/delete", convertFormToJson.fields([]), (req, res) => {
+  const { id } = req.body;
+  const params = {
+    TableName: tableName,
+    Key: {
+      id: Number(id),
+    },
+  };
+  docClient.delete(params, (err, data) => {
+    if (err) return res.send("err");
+    else return res.redirect("/");
   });
 });
 
